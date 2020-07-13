c5ae62a69607825708c80a9f26b3dc0bb205e36e
zhouwei <ntzw.geek@gmail.com>
2020-07-13 16:56:32 +0800

    -

--- admin/src/pages/cms/columnlist/components/modelfieldadd.tsx
+++ admin/src/pages/cms/columnlist/components/modelfieldadd.tsx
@@ -18,6 +18,7 @@ const RequiredSwitch = ({ key = 'required' }: { key?: string }) => {
         name="required"
         label="是否必填"
         valuePropName="checked"
+        
     >
         <Switch checkedChildren="是" unCheckedChildren="否" />
     </Form.Item>
